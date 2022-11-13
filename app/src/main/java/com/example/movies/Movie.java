59597256c4a26792fe17187ead09865aa0eb63c3
zmr89 <marsell.ru@gmail.com>
2022-11-13 20:41:23 +0500

    POJO created

---
+++ app/src/main/java/com/example/movies/Movie.java
@@ -0,0 +1,57 @@
+package com.example.movies;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Movie {
+
+    @SerializedName("id")
+    private int id;
+
+    @SerializedName("name")
+    private String name;
+
+    @SerializedName("description")
+    private String description;
+
+    @SerializedName("year")
+    private int year;
+
+    @SerializedName("poster")
+    private Poster poster;
+
+    @SerializedName("rating")
+    private Rating rating;
+
+    public Movie(int id, String name, String description, int year, Poster poster, Rating rating) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.year = year;
+        this.poster = poster;
+        this.rating = rating;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public Poster getPoster() {
+        return poster;
+    }
+
+    public Rating getRating() {
+        return rating;
+    }
+}
---
+++ app/src/main/java/com/example/movies/MovieResponse.java
@@ -0,0 +1,20 @@
+package com.example.movies;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MovieResponse {
+
+    @SerializedName("docs")
+    private List<Movie> movies = new ArrayList<>();
+
+    public MovieResponse(List<Movie> movies) {
+        this.movies = movies;
+    }
+
+    public List<Movie> getMovies() {
+        return movies;
+    }
+}
