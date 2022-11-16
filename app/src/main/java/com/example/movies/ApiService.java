86dc023ec971c9cfefe5b8c71c0504c013400819
zmr89 <marsell.ru@gmail.com>
2022-11-16 20:59:30 +0500

    Downloading new data

--- app/src/main/java/com/example/movies/ApiService.java
+++ app/src/main/java/com/example/movies/ApiService.java
@@ -6,6 +6,6 @@ import retrofit2.http.Query;
 
 public interface ApiService {
 
-    @GET("movie?token=XYV3HPG-1SB4PV1-JB06YPA-P2PMZJ5&field=rating.kp&search=7-10&sortField=votes.kp&sortType=-1&limit=4")
+    @GET("movie?token=XYV3HPG-1SB4PV1-JB06YPA-P2PMZJ5&field=rating.kp&search=7-10&sortField=votes.kp&sortType=-1&limit=30")
     Single<MovieResponse> loadMovieResponse(@Query("page") int page);
 }
--- app/src/main/java/com/example/movies/MainViewModel.java
+++ app/src/main/java/com/example/movies/MainViewModel.java
@@ -13,6 +13,7 @@ import java.util.List;
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
 import io.reactivex.rxjava3.disposables.CompositeDisposable;
 import io.reactivex.rxjava3.disposables.Disposable;
+import io.reactivex.rxjava3.functions.Action;
 import io.reactivex.rxjava3.functions.Consumer;
 import io.reactivex.rxjava3.schedulers.Schedulers;
 
@@ -21,20 +22,46 @@ public class MainViewModel extends AndroidViewModel {
     private int page = 1;
     private static final String TAG = "MainViewModel";
     private final MutableLiveData<List<Movie>> moviesLD = new MutableLiveData<>();
+    private final MutableLiveData<Boolean> isLoadingLD = new MutableLiveData<>(false);
     private final CompositeDisposable compositeDisposable = new CompositeDisposable();
 
     public MainViewModel(@NonNull Application application) {
         super(application);
+        loadMovies();
     }
 
     public void loadMovies(){
+        Boolean isLoading = isLoadingLD.getValue();
+        if (isLoading != null && isLoading){
+            return;
+        }
+
         Disposable disposable = ApiServiceFactory.API_SERVICE.loadMovieResponse(page)
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
+                .doOnSubscribe(new Consumer<Disposable>() {
+                    @Override
+                    public void accept(Disposable disposable) throws Throwable {
+                        isLoadingLD.setValue(true);
+                    }
+                })
+                .doAfterTerminate(new Action() {
+                    @Override
+                    public void run() throws Throwable {
+                        isLoadingLD.setValue(false);
+                    }
+                })
                 .subscribe(new Consumer<MovieResponse>() {
                     @Override
                     public void accept(MovieResponse movieResponse) throws Throwable {
-                        moviesLD.setValue(movieResponse.getMovies());
+                        List<Movie> listAllMovies = moviesLD.getValue();
+                        if (listAllMovies != null){
+                            listAllMovies.addAll(movieResponse.getMovies());
+                            moviesLD.setValue(listAllMovies);
+                        } else {
+                            moviesLD.setValue(movieResponse.getMovies());
+                        }
+                        Log.d("MainViewModel","page " + page);
                         page++;
                     }
                 }, new Consumer<Throwable>() {
@@ -48,6 +75,10 @@ public class MainViewModel extends AndroidViewModel {
 
     public LiveData<List<Movie>> getMoviesLD() {
         return moviesLD;
+    }
+
+    public LiveData<Boolean> getIsLoadingLD() {
+        return isLoadingLD;
     }
 
     @Override
